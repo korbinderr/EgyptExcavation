608d97baa1d0efa3b0c57871d7635ab4cdb0d7c7
Aaron Petersen <aaronp5@byu.edu>
2023-04-12 11:52:47 -0600

    My model

---
+++ Intex00/Models/Burialall3.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Intex00.Models
+{
+    public partial class Burialall3
+    {
+        public string Id { get; set; }
+        public string Squarenorthsouth { get; set; }
+        public string Headdirection { get; set; }
+        public string Sex { get; set; }
+        public string Northsouth { get; set; }
+        public string Depth { get; set; }
+        public string Eastwest { get; set; }
+        public string Adultsubadult { get; set; }
+        public string Facebundles { get; set; }
+        public string Southtohead { get; set; }
+        public string Preservation { get; set; }
+        public string Fieldbookpage { get; set; }
+        public string Squareeastwest { get; set; }
+        public string Goods { get; set; }
+        public string Text { get; set; }
+        public string Wrapping { get; set; }
+        public string Haircolor { get; set; }
+        public string Westtohead { get; set; }
+        public string Samplescollected { get; set; }
+        public string Area { get; set; }
+        public string Burialid { get; set; }
+        public string Length { get; set; }
+        public string Burialnumber { get; set; }
+        public string Dataexpertinitials { get; set; }
+        public string Westtofeet { get; set; }
+        public string Ageatdeath { get; set; }
+        public string Southtofeet { get; set; }
+        public string Excavationrecorder { get; set; }
+        public string Photos { get; set; }
+        public string Hair { get; set; }
+        public string Burialmaterials { get; set; }
+        public string Dateofexcavation { get; set; }
+        public string Fieldbookexcavationyear { get; set; }
+        public string Clusternumber { get; set; }
+        public string Shaftnumber { get; set; }
+        public string MainBurialmainid { get; set; }
+        public string MainTextileid { get; set; }
+        public string Id2 { get; set; }
+        public string Locale { get; set; }
+        public string Textileid { get; set; }
+        public string Description { get; set; }
+        public string Burialnumber2 { get; set; }
+        public string Estimatedperiod { get; set; }
+        public string Sampledate { get; set; }
+        public string Photographeddate { get; set; }
+        public string Direction { get; set; }
+        public string MainColorid { get; set; }
+        public string MainTextileid2 { get; set; }
+        public string Id3 { get; set; }
+        public string Value { get; set; }
+        public string Colorid { get; set; }
+        public string MainAnalysisid { get; set; }
+        public string MainTextileid3 { get; set; }
+        public string Id4 { get; set; }
+        public string Analysistype { get; set; }
+        public string Doneby { get; set; }
+        public string Analysisid { get; set; }
+        public string Date { get; set; }
+        public string MainDecorationid { get; set; }
+        public string MainTextileid4 { get; set; }
+        public string Id5 { get; set; }
+        public string Decorationid { get; set; }
+        public string Value2 { get; set; }
+        public string MainDimensionid { get; set; }
+        public string MainTextileid5 { get; set; }
+        public string Id6 { get; set; }
+        public string Dimensiontype { get; set; }
+        public string Value3 { get; set; }
+        public string Dimensionid { get; set; }
+        public string MainStructureid { get; set; }
+        public string MainTextileid6 { get; set; }
+        public string Id7 { get; set; }
+        public string Value4 { get; set; }
+        public string Structureid { get; set; }
+        public string MainTextilefunctionid { get; set; }
+        public string MainTextileid7 { get; set; }
+        public string Id8 { get; set; }
+        public string Value5 { get; set; }
+        public string Textilefunctionid { get; set; }
+        public string MainYarnmanipulationid { get; set; }
+        public string MainTextileid8 { get; set; }
+        public string Id9 { get; set; }
+        public string Thickness { get; set; }
+        public string Angle { get; set; }
+        public string Manipulation { get; set; }
+        public string Material { get; set; }
+        public string Count { get; set; }
+        public string Component { get; set; }
+        public string Ply { get; set; }
+        public string Yarnmanipulationid { get; set; }
+        public string Direction2 { get; set; }
+    }
+}
