c30000d68e3774c543404d9e84d49614e47d41fd
Emma Plessinger <plessinger.em@gmail.com>
2023-04-13 18:39:06 -0600

    css stuffs

--- Intex00/Controllers/apiController.cs
+++ Intex00/Controllers/apiController.cs
@@ -1,36 +1,36 @@
-﻿//using Intex00.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.ML.OnnxRuntime;
-//using Microsoft.ML.OnnxRuntime.Tensors;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Intex00.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace Intex00.Controllers
-//{
-//    [ApiController]
-//    [Route("/predict")]
-//    public class apiController : Controller
-//    {
-//        private InferenceSession _session;
+namespace Intex00.Controllers
+{
+    [ApiController]
+    [Route("/predict")]
+    public class apiController : Controller
+    {
+        private InferenceSession _session;
 
-//        public apiController(InferenceSession session)
-//        {
-//            _session = session;
-//        }
+        public apiController(InferenceSession session)
+        {
+            _session = session;
+        }
 
-//        [HttpPost]
-//        public ActionResult Score(WrappingData data)
-//        {
-//            var result = _session.Run(new List<NamedOnnxValue>
-//            {
-//                NamedOnnxValue.CreateFromTensor("float_input", data.AssTensor())
-//            });
-//            Tensor<string> score = result.First().AsTensor<string>();
-//            var prediction = new predictions { PredictedValue = score.First() };
-//            result.Dispose();
-//            return Ok(prediction);
-//        }
-//    }
-//}
+        [HttpPost]
+        public ActionResult Score(WrappingData data)
+        {
+            var result = _session.Run(new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("float_input", data.AssTensor())
+            });
+            Tensor<string> score = result.First().AsTensor<string>();
+            var prediction = new predictions { PredictedValue = score.First() };
+            result.Dispose();
+            return Ok(prediction);
+        }
+    }
+}
